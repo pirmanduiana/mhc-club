5e6aa8a60092acb52ab8c3b1c4bb585db0d2d290
I Made Pirman Duiana <pirmandwiana@gmail.com>
2021-07-04 04:42:28 +0800

    fjkdl

--- app/Http/Controllers/SwabController.php
+++ app/Http/Controllers/SwabController.php
@@ -10,10 +10,15 @@ class SwabController extends Controller
     public function getHasilSwab($no_identitas)
     {
         $data = Swabs::where('no_identitas', $no_identitas)->with('dokter','kelamin','hasil')->first();
-        $qrcode = "https://mhc-club.com/swab/$data->no_identitas";
-        $ispdf = 0;
-        $show_tools_button = 0;
+        
+        if (!is_null($data)) {
+            $qrcode = "https://mhc-club.com/swab/$data->no_identitas";
+            $ispdf = 0;
+            $show_tools_button = 0;
+    
+            return view('swab.hasil', compact('data','qrcode','ispdf','show_tools_button'));
+        }
 
-        return view('swab.hasil', compact('data','qrcode','ispdf','show_tools_button'));
+        abort(404);
     }
 }
